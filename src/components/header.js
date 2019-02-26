5fe36c2b080c6cfd6de51cb3aa2e3ec6c7c199b8
ANVESH96 <anveshreddymekala333@gmail.com>
2019-02-26 01:01:38 -0500

    Header updated

--- src/components/header.js
+++ src/components/header.js
@@ -5,15 +5,15 @@ import "../styles/bootstrap.min.css"
 import {Nav,Navbar,Row,Col} from "react-bootstrap"
 
 const Header = ({ siteTitle }) => (
-  <Navbar  sticky ="top"  bg="dark" expand="md" variant="dark" role="navigation" style={{minHeight:90,fontFamily:'lato',color:'white'}}>
-  <Navbar.Brand  >{'ANVESHREDDY MEKALA'}</Navbar.Brand>
+  <Navbar  sticky ="top"  bg="dark" expand="lg" variant="dark" role="navigation" style={{minHeight:90,fontFamily:'lato',color:'white'}}>
+  <Navbar.Brand style={{padding:"0rem 0rem 0rem 7rem"}} >{'ANVESHREDDY MEKALA'}</Navbar.Brand>
   <Navbar.Toggle aria-controls="basic-navbar-nav" /> 
    <Navbar.Collapse id="basic-navbar-nav">
-     <Nav expand="md" className="mr-auto" >
-       <Nav> <Link to={"/"} style={{color:'white'}}> HOME </Link> </Nav>
-       <Nav><Link to={"/skills"} style={{color:'white'}}>SKILLS </Link></Nav>
-       <Nav><Link to={"/projects"} style={{color:'white'}}>PROJECTS</Link></Nav>
-       <Nav><Link to={"/contact"} style={{color:'white'}}>CONTACT </Link></Nav>
+     <Nav expand="md"  className="mr-auto" >
+       <Nav style={{padding:"0rem 0rem 0rem 7rem"}} > <Link to={"/"} style={{color:'white'}}> HOME </Link> </Nav>
+       <Nav style={{padding:"0rem 0rem 0rem 7rem"}}><Link to={"/skills"} style={{color:'white'}}>SKILLS </Link></Nav>
+       <Nav style={{padding:"0rem 0rem 0rem 7rem"}}><Link to={"/projects"} style={{color:'white'}}>PROJECTS</Link></Nav>
+       <Nav  style={{padding:"0rem 0rem 0rem 7rem"}}><Link to={"/contact"} style={{color:'white'}}>CONTACT </Link></Nav>
      </Nav>
      </Navbar.Collapse>
    </Navbar>
